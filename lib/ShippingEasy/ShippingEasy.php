d61d2558f115a990aa0eff0d5b04671b2ffc2de5
Theo Mills <twmills@twmills.com>
2014-01-28 15:19:40 -0600

    Switch out localhost URL.

--- lib/ShippingEasy/ShippingEasy.php
+++ lib/ShippingEasy/ShippingEasy.php
@@ -4,7 +4,7 @@ abstract class ShippingEasy
 {
   public static $apiKey;
   public static $apiSecret;
-  public static $apiBase = 'http://localhost:5000';
+  public static $apiBase = 'https://app.shippingeasy.com';
   public static $apiVersion = null;
   const VERSION = '0.0.1';
 
