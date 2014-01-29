a41ab6403b2f36df9a478b9e6adb865cf9699ce1
Theo Mills <twmills@twmills.com>
2014-01-29 08:34:22 -0600

    Switch to passing in attrs in constructor.

--- lib/ShippingEasy/Cancellation.php
+++ lib/ShippingEasy/Cancellation.php
@@ -2,9 +2,14 @@
 
 class ShippingEasy_Cancellation extends ShippingEasy_Object
 {  
-  public function create($external_order_identifier)
+  
+  public function __construct($external_order_identifier) {
+    $this->$external_order_identifier = $external_order_identifier;
+  }
+  
+  public function create()
   {
-    $this->request("post", "/api/orders/$external_order_identifier/cancellations");
+    $this->request("post", "/api/orders/$this->external_order_identifier/cancellations");
   }
   
 }
