e911e36ae91d879cd69645bf85b9f29ebce263cc
Theo Mills <twmills@twmills.com>
2014-02-17 10:30:24 -0600

    Fix variable reference.

--- lib/ShippingEasy/Cancellation.php
+++ lib/ShippingEasy/Cancellation.php
@@ -1,15 +1,15 @@
 <?php
 
 class ShippingEasy_Cancellation extends ShippingEasy_Object
-{  
-  
+{
+
   public function __construct($external_order_identifier) {
-    $this->$external_order_identifier = $external_order_identifier;
+    $this->external_order_identifier = $external_order_identifier;
   }
-  
+
   public function create()
   {
     $this->request("post", "/api/orders/$this->external_order_identifier/cancellations");
   }
-  
+
 }
